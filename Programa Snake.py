95203f1d363c818ce70330b6cad6b651ed384971
KethelenTamara <kettamara@gmail.com>
2017-06-15 13:37:22 -0400

    Create Programa Snake.py

--- Programa Snake.py
+++ Programa Snake.py
@@ -1,3 +1,20 @@
+#Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# 
+# Aracille de Souza Barbosa                     1315120206
+# Diego Reis Figueira                           1515070169
+# João Victor de Cordeiro                       1515140036
+# Kethelen Tamara Braga Barbosa                 1525212002
+# Marcus Vinicius Paes da Silva Santos          1515070060
+# Ulisses Antonio Antonino da Costa             1515090555
+
+#-----------------------------------------------------------------
+
+
+
+
+
 import pygame
 import random
 import sys
